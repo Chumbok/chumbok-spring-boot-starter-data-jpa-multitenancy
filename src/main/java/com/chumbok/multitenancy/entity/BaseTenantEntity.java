fe58d2d2e2c33fb03d2a35c09b43592f58fa2fcd
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2018-10-08 21:36:20 +0200

    Added org tenant filter on entity.

--- src/main/java/com/chumbok/multitenancy/entity/BaseTenantEntity.java
+++ src/main/java/com/chumbok/multitenancy/entity/BaseTenantEntity.java
@@ -6,9 +6,16 @@ import com.chumbok.multitenancy.annotation.OrgIdentifier;
 import com.chumbok.multitenancy.annotation.TenantIdentifier;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.FilterDefs;
+import org.hibernate.annotations.Filters;
+import org.hibernate.annotations.ParamDef;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.MappedSuperclass;
 
 /**
@@ -17,6 +24,12 @@ import javax.persistence.MappedSuperclass;
 @Getter
 @Setter
 @MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@FilterDefs({
+        @FilterDef(name = "orgFilter", parameters = @ParamDef(name = "org", type = "string"), defaultCondition = "org = :org"),
+        @FilterDef(name = "tenantFilter", parameters = @ParamDef(name = "tenant", type = "string"), defaultCondition = "tenant = :tenant")
+})
+@Filters({@Filter(name = "orgFilter"), @Filter(name = "tenantFilter")})
 @EntityListeners(TenantEntityListener.class)
 public abstract class BaseTenantEntity implements TenantAwareEntity<String> {
 
