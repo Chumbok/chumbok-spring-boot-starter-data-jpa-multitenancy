6453f948447fdadd8e71a3e82d8832249aff8196
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2018-10-11 03:27:04 +0200

    Update integration tests.

--- src/integration-test/java/com/chumbok/multitenancy/DataJpaMultitenancyIT.java
+++ src/integration-test/java/com/chumbok/multitenancy/DataJpaMultitenancyIT.java
@@ -1,37 +1,221 @@
 package com.chumbok.multitenancy;
 
+import com.chumbok.multitenancy.annotation.EnableMultitenantJpaRepositories;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = TestApplication.class)
+@AutoConfigureMockMvc
 @ActiveProfiles("it")
 public class DataJpaMultitenancyIT {
 
     @Autowired
     private HelloRepository helloRepository;
 
+    @Autowired
+    private TenantAware tenantAware;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
     @Test
-    public void shouldMultitenancyFieldsBePopulated() {
+    public void shouldFetchEntitiesThatBelongsToUsersOrgTenant() throws Exception {
+
+        // User1 create/saves Hello resource
+        mimicUser("User1");
+        mockMvc.perform(post("/hello")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"id\": \"User1UUID\", \"message\": \"User1SampleMessage\" }"))
+                .andExpect(jsonPath("$.id").value("User1UUID"))
+                .andExpect(jsonPath("$.message").value("User1SampleMessage"))
+                .andExpect(jsonPath("$.org").value("User1Org"))
+                .andExpect(jsonPath("$.tenant").value("User1Tenant"))
+                .andDo(print());
+
+        // User2 create/saves Hello resource
+        mimicUser("User2");
+        mockMvc.perform(post("/hello")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"id\": \"User2UUID\", \"message\": \"User2SampleMessage\" }"))
+                .andExpect(jsonPath("$.id").value("User2UUID"))
+                .andExpect(jsonPath("$.message").value("User2SampleMessage"))
+                .andExpect(jsonPath("$.org").value("User2Org"))
+                .andExpect(jsonPath("$.tenant").value("User2Tenant"))
+                .andDo(print());
+
+        // Assert if User1 gets only one Hello resource
+        mimicUser("User1");
+        mockMvc.perform(get("/hello"))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id").value("User1UUID"))
+                .andExpect(jsonPath("$[0].message").value("User1SampleMessage"))
+                .andExpect(jsonPath("$[0].org").value("User1Org"))
+                .andExpect(jsonPath("$[0].tenant").value("User1Tenant"))
+                .andDo(print());
+
+        // Assert if User2 gets only one Hello resource
+        mimicUser("User2");
+        mockMvc.perform(get("/hello"))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id").value("User2UUID"))
+                .andExpect(jsonPath("$[0].message").value("User2SampleMessage"))
+                .andExpect(jsonPath("$[0].org").value("User2Org"))
+                .andExpect(jsonPath("$[0].tenant").value("User2Tenant"))
+                .andDo(print());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenOneUserTriesToUpdateAnotherUsersResource() throws Exception {
+
+        // User1 create/saves Hello resource
+        mimicUser("User3");
+        mockMvc.perform(post("/hello")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"id\": \"User3UUID\", \"message\": \"User3SampleMessage\" }"))
+                .andExpect(jsonPath("$.id").value("User3UUID"))
+                .andExpect(jsonPath("$.message").value("User3SampleMessage"))
+                .andExpect(jsonPath("$.org").value("User3Org"))
+                .andExpect(jsonPath("$.tenant").value("User3Tenant"))
+                .andDo(print());
+
+        mimicUser("User4");
+        mockMvc.perform(put("/hello/User3UUID")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"id\": \"User3UUID\", \"message\": \"User3SampleMessage\" }"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.message").value("Hello[User3UUID] does not exist."))
+                .andDo(print());
+
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenOneUserTriesToDeleteAnotherUsersResource() throws Exception {
+
+        // User1 create/saves Hello resource
+        mimicUser("User5");
+        mockMvc.perform(post("/hello")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"id\": \"User5UUID\", \"message\": \"User5SampleMessage\" }"))
+                .andExpect(jsonPath("$.id").value("User5UUID"))
+                .andExpect(jsonPath("$.message").value("User5SampleMessage"))
+                .andExpect(jsonPath("$.org").value("User5Org"))
+                .andExpect(jsonPath("$.tenant").value("User5Tenant"))
+                .andDo(print());
+
+        mimicUser("User6");
+        mockMvc.perform(delete("/hello/User5UUID"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.message").value("Hello[User5UUID] does not exist."))
+                .andDo(print());
+
+    }
+
+    @Test
+    public void shouldNotAbleToFetchOtherUsersResource() throws Exception {
+
+        // User1 create/saves Hello resource
+        mimicUser("User7");
+        mockMvc.perform(post("/hello")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"id\": \"User7UUID\", \"message\": \"User7SampleMessage\" }"))
+                .andExpect(jsonPath("$.id").value("User7UUID"))
+                .andExpect(jsonPath("$.message").value("User7SampleMessage"))
+                .andExpect(jsonPath("$.org").value("User7Org"))
+                .andExpect(jsonPath("$.tenant").value("User7Tenant"))
+                .andDo(print());
+
+        mimicUser("User8");
+        mockMvc.perform(get("/hello/User7UUID"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.message").value("Hello[User7UUID] does not exist."))
+                .andDo(print());
+
+    }
+
+    @Test
+    public void shouldPopulatedOrgAndTenantFields() {
 
         // Given
+
+        mimicUser("Test");
+
         Hello unsaved = new Hello();
-        unsaved.setId("uuid");
+        unsaved.setId("uuid" + UUID.randomUUID());
         unsaved.setMessage("sampleMessage");
 
         // When
-        Hello saved = helloRepository.save(unsaved);
+        Hello saved = helloRepository.saveAndFlush(unsaved);
 
         // Then
         assertEquals("TestOrg", saved.getOrg());
         assertEquals("TestTenant", saved.getTenant());
+
     }
 
+    private void mimicUser(String user) {
+        ((JpaTenantTestConfig.TenantAwareImpl) tenantAware).userIdentifier(user);
+    }
 
+    @Configuration
+    @EnableMultitenantJpaRepositories
+    static class JpaTenantTestConfig {
+
+        @Bean
+        public TenantAware<String> tenantAware() {
+            return new TenantAwareImpl();
+        }
+
+        static class TenantAwareImpl implements TenantAware<String> {
+
+            private String user;
+
+            public void userIdentifier(String user) {
+                this.user = user;
+            }
+
+            @Override
+            public Optional<String> getOrg() {
+                return Optional.of(user + "Org");
+            }
+
+            @Override
+            public Optional<String> getTenant() {
+                return Optional.of(user + "Tenant");
+            }
+        }
+    }
 }
--- src/integration-test/java/com/chumbok/multitenancy/TestApplication.java
+++ src/integration-test/java/com/chumbok/multitenancy/TestApplication.java
@@ -3,16 +3,22 @@ package com.chumbok.multitenancy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @SpringBootApplication
 @PropertySource("classpath:application.yml")
@@ -32,8 +38,15 @@ public class TestApplication {
     }
 
     @GetMapping("/hello/{id}")
-    public void getHello(@PathVariable String id) {
-        helloRepository.findById(id);
+    public Hello getHello(@PathVariable String id) {
+
+        Optional<Hello> hello = helloRepository.findById(id);
+
+        if (!hello.isPresent()) {
+            throw new RuntimeException("Hello[" + id + "] does not exist.");
+        }
+
+        return hello.get();
     }
 
 
@@ -44,12 +57,34 @@ public class TestApplication {
 
     @PutMapping("/hello/{id}")
     public Hello updateHello(@PathVariable String id, @RequestBody Hello hello) {
-        return helloRepository.saveAndFlush(hello);
+
+        Optional<Hello> savedHello = helloRepository.findById(id);
+
+        if (!savedHello.isPresent()) {
+            throw new RuntimeException("Hello[" + id + "] does not exist.");
+        }
+
+        savedHello.get().setMessage(hello.getMessage());
+        return helloRepository.saveAndFlush(savedHello.get());
     }
 
     @DeleteMapping("/hello/{id}")
     public void deleteHello(@PathVariable String id) {
+
+        Optional<Hello> savedHello = helloRepository.findById(id);
+
+        if (!savedHello.isPresent()) {
+            throw new RuntimeException("Hello[" + id + "] does not exist.");
+        }
+
         helloRepository.deleteById(id);
+    }
+
+    // Returning INTERNAL_SERVER_ERROR since it just a test
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(RuntimeException.class)
+    public Map<String, String> handleException(RuntimeException ex) {
+        return Collections.singletonMap("message", ex.getMessage());
     }
 }
 
